ff73ddc8b908bfe45b3d5cfec8140513b40a2198
prachi-ramane <66133562+prachi-ramane@users.noreply.github.com>
2022-07-13 12:26:35 +0530

    second commit

---
+++ StudentInformationSystem/src/model/Customer.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Customer {
+	String custID;
+	String name;
+	public String getCustID() {
+		return custID;
+	}
+	public void setCustID(String custID) {
+		this.custID = custID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
