3290999ceabe01d4fe87830f6a17a7293043e763
prachi-ramane <66133562+prachi-ramane@users.noreply.github.com>
2022-07-13 13:36:33 +0530

    Added Account model to branch 1

---
+++ StudentInformationSystem/src/model/Account.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Account {
+int accNo;
+int balance;
+public int getAccNo() {
+	return accNo;
+}
+public void setAccNo(int accNo) {
+	this.accNo = accNo;
+}
+public int getBalance() {
+	return balance;
+}
+public void setBalance(int balance) {
+	this.balance = balance;
+}
+
+}
